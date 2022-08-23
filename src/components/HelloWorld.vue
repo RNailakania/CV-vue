9620af8dfec11973bc84e7291e9f58253e885685
RNailakania <baguskania3@gmail.com>
2022-08-23 11:15:29 +0700

    commit cv

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Software Developer
+      <p><a target="_blank" href="https://instagram.com/inikaniia_?igshid=YmMyMTA2M2Y=">@inikaniia_</a></p> 
+      <p> <a target="_blank" href="https://github.com/RNailakania">RNailakania</a></p>
+      <p> <a target="_blank" href="https://vuejs.org/">nailakaniawidia@gmail.com</a></p>
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+    <h3 style="font-weight: 700; margin-top: 30px;">About Me</h3>
+    Kania is a student at Taruna Bhakti Vocational High School, majoring in Software Engineering.
+
+    To improve her abilites, she is constantly developer her knowledge and skills for further working experience and make good impact for his carrier.
+
+
+    <h3 style="font-weight: 700; margin-top: 30px;">Training & Seminar</h3>
+    Level test and LSP test, Digital literacy Seminar, Sharing session creative entrepreneur, Digital business webinar, IT talk series "The Latest Technology for Software Developer" seminar
+ 
+    <h3 style="font-weight: 700; margin-top: 30px;">Educational background</h3>
+    SMK Taruna Bhakti, Major in Software Engineering — 2020-2023
+
+    <h3 style="font-weight: 700; margin-top: 30px;">Contact Us</h3>
+    08128523627
+
+</template>
